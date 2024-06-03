f9372aca4d8827e336b6f5622e05e09cd63871ea
Ramon Silva <ramon.silva.ismart@gmail.com>
2024-06-03 18:09:27 -0300

    bug fix

--- src/components/list-todo/list-todo.tsx
+++ src/components/list-todo/list-todo.tsx
@@ -2,15 +2,15 @@ import { ShowTodo } from "@components/show-todo/show-todo"
 import { ListTodoProps } from "./list-todo.types"
 
 export const ListTodo: React.FC<ListTodoProps> = ({ todos, filterBy }) => {
-  if (todos.length === 0) {
+  const visibleTodos = todos.filter((todo) => !filterBy || todo.status === filterBy)
+  if (visibleTodos.length === 0) {
     return (
-      <p>No todos yet</p>
+      <p>No { filterBy } todos yet</p>
     )
   }
   return (
     <ul>
       {todos
-      .filter((todo) => !filterBy || todo.status === filterBy)
       .map((todo) => (
         <li key={todo.id}>
           <ShowTodo todo={todo} />
